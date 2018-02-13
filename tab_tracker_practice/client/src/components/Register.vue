eef6c2940fdbb3480d27cdd3b494111dfbb406f0
Josh Barber <joshbarber17@me.com>
2018-02-13 11:49:29 -0700

    successful front and back end with endpoints being hit correctly. much more to come

---
+++ tab_tracker_practice/client/src/components/Register.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <span>Registering</span>
+    <br>
+    <input type='email' name='email' placeholder='email' v-model='email'/>
+    <input type='password' name='password' placeholder='password' v-model='password'/>
+    <br>
+    <button @click='register'>Register</button>
+  </div>
+</template>
+<script>
+import AuthenticationService from '@/services/authenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  }
+}
+</script>
